1437913124889901d98a71728dde51bc3beb64a6
lockust <paris.papakonstantinou@gmail.com>
2018-07-25 22:16:00 +0100

    Add files via upload

---
+++ BasicAnalyticsEngine/Frequencies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAnalyticsEngine
+{
+    class Frequencies
+    {
+        public int LowerBound{get;set;}
+        public int UpperBound { get; set; }
+        public double Freq { get; set; }
+    }
+}
